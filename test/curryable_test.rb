a0653c128228ef05432c7bc93c26c33b8bfc0cdb
Thiago A. Silva <thiagoaraujos@gmail.com>
2014-07-17 15:18:27 -0300

    More understable curryable_test

--- test/curryable_test.rb
+++ test/curryable_test.rb
@@ -1,28 +1,46 @@
 require 'minitest/autorun'
 require_relative '../curryable'
 
-describe Curryable do
-  describe "my_curry runs with no arguments" do
+describe "a curried proc" do
+  describe "when my_curry isn't called on a proc" do
     before do
-      @fn = proc { |a, b, c| a + b + c }
+      @fn = proc { |a, b, c, d| a + b + c + d }
     end
 
-    it "is curried" do
-      @fn.my_curry.curried?.must_equal true
+    it "isn't curried" do
+      @fn.curried?.must_equal false
+    end
+
+    it 'has the right arity' do
+      @fn.arity.must_equal 4
+    end
+  end
+
+  describe "when my_curry is called on a proc, with no arguments" do
+    before do
+      @fn = proc { |a, b, c| a + b + c }.my_curry
+    end
+
+    it "returns a new curried proc" do
+      @fn.curried?.must_equal true
     end
 
     it "has the right arity" do
-      @fn.my_curry.arity.must_equal 3
+      @fn.arity.must_equal 3
     end
   end
 
   describe "with a proc of one argument" do
+    before do
+      @fn = proc { |a| a }
+    end
+
     it "my_curry returns the final result" do
-      proc { |a| a }.my_curry(1).must_equal 1
+      @fn.my_curry(1).must_equal 1
     end
 
     it "call returns the final result" do
-      proc { |a| a }.call(1).must_equal 1
+      @fn.call(1).must_equal 1
     end
   end
 
@@ -31,18 +49,12 @@ describe Curryable do
       @fn = proc { |a, b, c, d| a + b + c + d }
     end
 
-    describe 'when my_curry is not called' do
-      it "isn't curried" do
-        @fn.curried?.must_equal false
-      end
-    end
-
-    describe "after my_curry is called with one argument" do
+    describe "when my_curry is called with one argument" do
       before do
         @fn = @fn.my_curry(1)
       end
 
-      it 'is curried' do
+      it 'returns a new curried proc' do
         @fn.curried?.must_equal true
       end
 
@@ -50,8 +62,8 @@ describe Curryable do
         @fn.arity.must_equal 3
       end
 
-      describe "when my_curry is called once more with one argument" do
-        it "is curried" do
+      describe "when my_curry is called once more, with one argument" do
+        it "returns a new curried proc" do
           @fn.my_curry(2).curried?.must_equal true
         end
 
@@ -60,8 +72,8 @@ describe Curryable do
         end
       end
 
-      describe "when my_curry is called once more with two arguments" do
-        it "is curried" do
+      describe "when my_curry is called once more, with two arguments" do
+        it "returns a new curried proc" do
           @fn.my_curry(2, 3).curried?.must_equal true
         end
 
@@ -71,7 +83,7 @@ describe Curryable do
       end
 
       describe "when my_curry is called twice more, each with one argument" do
-        it "is curried" do
+        it "returns a new curried proc" do
           @fn.my_curry(2).my_curry(3).curried?.must_equal true
         end
 
@@ -80,27 +92,27 @@ describe Curryable do
         end
       end
 
-      describe "when my_curry is called once more with three arguments" do
+      describe "when my_curry is called once more, with three arguments" do
         it "returns the final result" do
           @fn.my_curry(2, 3, 4).must_equal 10
         end
       end
 
       describe "when my_curry is called thrice more, each with one argument" do
-        it "returns the final proc result" do
+        it "returns the final result" do
           @fn.my_curry(2).my_curry(3).my_curry(4).must_equal 10
         end
       end
     end
 
     describe "when my_curry is called with all four arguments" do
-      it "returns the final proc result" do
+      it "returns the final result" do
         @fn.my_curry(1, 2, 3, 4).must_equal 10
       end
     end
   end
 
-  describe "once my_curry is triggered once, the call method behaves like my_curry" do
+  describe "when my_curry is triggered once, the call method behaves like my_curry" do
     it do
       fn = proc { |a, b, c, d| a + b + c + d }
 
